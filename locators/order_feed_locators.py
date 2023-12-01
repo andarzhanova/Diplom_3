e09245b20953e11c45e1609430e1fbd6ad0e3f22
Anna Andarzhanova <a.andarjanova@yandex.ru>
2023-12-02 02:21:26 +0300

    Fix locators

--- locators/order_feed_locators.py
+++ locators/order_feed_locators.py
@@ -6,8 +6,10 @@ class OrderFeedLocators:
     CONSTRUCTOR_BUTTON = (By.XPATH, '//p[text()="Конструктор"]')  # Кнопка "Конструктор"
     ORDER = (By.XPATH, '//ul[@class="OrderFeed_list__OLh59"]/li[1]')  # Заказ
     POP_UP_WINDOW = (By.XPATH, '//div[contains(@class, "Modal_order")]')  # Всплывающее окно с деталями
-    ORDER_NUMBERS = (By.XPATH, '//ul//p[@class="text text_type_digits-default"]')  # Номера всех заказов в ленте
-    LAST_ORDER = (By.XPATH, '//ul/li[1]//p[@class="text text_type_digits-default"]')  # Номер последнего заказа в ленте
-    ALL_TIME_COUNTER = (By.CSS_SELECTOR, 'div.undefined p.OrderFeed_number__2MbrQ')  # Счётчик "Выполнено за всё время"
-    TODAY_COUNTER = (By.XPATH, '//div[3]/p[contains(@class, "digits")]')  # Счётчик "Выполнено за сегодня"
+    ORDER_NUMBERS = (By.XPATH, '//p[@class="text text_type_digits-default"]')  # Номера всех заказов в ленте
+    LAST_ORDER = (By.XPATH, '//li[1]//p[@class="text text_type_digits-default"]')  # Номер последнего заказа в ленте
+    # Счётчик "Выполнено за всё время"
+    ALL_TIME_COUNTER = (By.XPATH, '//p[text()="Выполнено за все время:"]/../p[contains(@class, "digits")]')
+    # Счётчик "Выполнено за сегодня"
+    TODAY_COUNTER = (By.XPATH, '//p[text()="Выполнено за сегодня:"]/../p[contains(@class, "digits")]')
     NUMBER_IN_PROGRESS = (By.CSS_SELECTOR, 'ul.OrderFeed_orderListReady__1YFem li')  # Номер в разделе "В работе"
--- locators/reset_password_locators.py
+++ locators/reset_password_locators.py
@@ -3,4 +3,4 @@ from selenium.webdriver.common.by import By
 
 class ResetPasswordLocators:
     SHOW_BUTTON = (By.XPATH, '//div[contains(@class,"input__icon")]/*')  # Кнопка "Показать пароль"
-    PASSWORD_FIELD = (By.XPATH, '//form/fieldset[1]//div[contains(@class,"input_size_default")]')  # Поле пароль
+    PASSWORD_FIELD = (By.XPATH, '//fieldset[1]//div[contains(@class,"input_size_default")]')  # Поле пароль
